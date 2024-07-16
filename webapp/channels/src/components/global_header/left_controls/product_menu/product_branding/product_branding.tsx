0a83b39df9492d9cfa65860e53b28a19896517f5
OptionsBuffetDev <mcrafter147@gmail.com>
2024-07-17 03:28:18 +0530

    [Fix] Channels button on global header keyboard selection

--- webapp/channels/src/components/global_header/left_controls/product_menu/product_branding/product_branding.tsx
+++ webapp/channels/src/components/global_header/left_controls/product_menu/product_branding/product_branding.tsx
@@ -1,13 +1,16 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
+import { useDispatch, useSelector } from 'react-redux';
 
 import Heading from '@mattermost/compass-components/components/heading'; // eslint-disable-line no-restricted-imports
 import glyphMap, {ProductChannelsIcon} from '@mattermost/compass-icons/components';
 
 import {useCurrentProduct} from 'utils/products';
+import { setProductMenuSwitcherOpen } from 'actions/views/product_menu';
+import { isSwitcherOpen } from 'selectors/views/product_menu';
 
 const ProductBrandingContainer = styled.div`
     display: flex;
@@ -19,12 +22,24 @@ const ProductBrandingContainer = styled.div`
 `;
 
 const ProductBranding = (): JSX.Element => {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
     const currentProduct = useCurrentProduct();
+    const dispatch = useDispatch();
+    const switcherOpen = useSelector(isSwitcherOpen);
+
+    const handleKeyDown = (e) => {
+        // also space key down can be added the same way
+        if (e.key === 'Enter') {
+            e.preventDefault();
+            setIsMenuOpen(!switcherOpen);
+            dispatch(setProductMenuSwitcherOpen(!switcherOpen));
+        }
+    };
 
     const Icon = currentProduct?.switcherIcon ? glyphMap[currentProduct.switcherIcon] : ProductChannelsIcon;
 
     return (
-        <ProductBrandingContainer tabIndex={0}>
+        <ProductBrandingContainer tabIndex={0} onKeyDown={handleKeyDown}>
             <Icon size={24}/>
             <Heading
                 element='h1'
